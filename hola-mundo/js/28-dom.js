cddab9d6459dfa9d825da62d03f1797fd352850a
Cristian Uhrig <cristianuhrig@gmail.com>
2021-05-14 11:17:01 -0300

    DOM pt2 seleccionando por ID, class y etiqueta

--- hola-mundo/js/28-dom.js
+++ hola-mundo/js/28-dom.js
@@ -6,6 +6,8 @@ function changeTextColor(color) {
 	caja.style.color = color;
 }
 
+// CONSEGUIR ELEMENTOS POR ID
+
 // Guardo en la variable caja mi elemento con id "miCaja"
 /* Metodo 1: */ // var caja = document.getElementById("miCaja");
 /* Metodo 2: 
@@ -24,4 +26,40 @@ caja.style.textTransform = "uppercase";
 // Le agrego una clase a mi caja
 caja.className = "texto texto2";
 
+// CONSEGUIR ELEMENTOS POR ETIQUETA HTML
+// Esto me genera un array con todos los elementos con etiqueta div de mi html
+var todosLosDivs = document.getElementsByTagName("div");
+todosLosDivs[1].innerHTML = "Soy el segundo DIV y me modificaron por JS";
+
+var valor;
+var seccion = document.querySelector("#miSeccion");
+var hr = document.createElement("hr");
+
+for (valor in todosLosDivs) {
+	if (todosLosDivs[valor].textContent) {
+		var parrafo = document.createElement("p");
+		var texto = document.createTextNode(todosLosDivs[valor].textContent);
+		parrafo.append(texto);
+		seccion.append(parrafo);
+	}
+}
+seccion.append(hr);
+for (let i = 0; i < todosLosDivs.length; i++) {
+	todosLosDivs[i].style.margin = "10px 0px";
+	todosLosDivs[i].style.backgroundColor = "blue";
+	todosLosDivs[i].style.color = "#fff";
+}
+
+// CONSEGUIR ELEMENTOS POR CLASE
+var divsRojos = document.getElementsByClassName("rojo");
+
+for (var div in divsRojos) {
+	if (divsRojos[div].className == "rojo") {
+		divsRojos[div].style.background = "red";
+	}
+}
+
+var divsAmarillos = document.getElementsByClassName("amarillo");
+divsAmarillos[0].style.background = "yellow";
+divsAmarillos[0].style.color = "black";
 console.log(caja);

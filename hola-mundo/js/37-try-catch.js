eebd669b376e2346434b91f22e2f9f3f2e6207e2
Cristian Uhrig <cristianuhrig@gmail.com>
2021-05-18 16:53:30 -0300

    try-catch

---
+++ hola-mundo/js/37-try-catch.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.addEventListener("load", () => {
+	// Try catch
+	try {
+		var year = 2021;
+		// console.log(yea);
+		console.log(decodeURIComponent("h12ttps:/&/&%google.com"));
+	} catch (error) {
+		console.log(error);
+	}
+});

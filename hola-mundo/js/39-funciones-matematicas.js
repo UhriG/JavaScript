a138c92e5bb183a847030cecab8558ef8fda9fb7
Cristian Uhrig <cristianuhrig@gmail.com>
2021-05-18 18:24:50 -0300

    .

--- hola-mundo/js/39-funciones-matematicas.js
+++ hola-mundo/js/39-funciones-matematicas.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// ver mas: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math
 window.addEventListener("load", () => {
 	console.log(Math.round(Math.random() * 1000));
 });

d1824f5cd72a8f8253ca42ccb4f82afae5dc5e61
Cristian Uhrig <cristianuhrig@gmail.com>
2021-05-18 21:19:49 -0300

    mouseouver, mouseout y hover

---
+++ jQuery/js/02-eventos.js
@@ -0,0 +1,26 @@
+$(function () {
+	// Eventos
+
+	var caja = $("#caja");
+
+	//* MouseOver y mouseOut
+
+	/* caja.mouseover(function () {
+		$(this).css("background-color", "blue");
+	});
+	caja.mouseout(function () {
+		$(this).css("background-color", "green");
+	}); */
+
+	//* Hover
+	caja.hover(
+		function () {
+			//* over
+			$(this).css("background-color", "blue");
+		},
+		function () {
+			//* out
+			$(this).css("background-color", "green");
+		}
+	);
+});

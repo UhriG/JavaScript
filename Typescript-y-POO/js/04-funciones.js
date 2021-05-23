7cdee24e9c74f37d04718a330ccc3bc65deaef3f
Cristian Uhrig <cristianuhrig@gmail.com>
2021-05-23 13:23:57 -0300

    funciones en ts

--- Typescript-y-POO/js/04-funciones.js
+++ Typescript-y-POO/js/04-funciones.js
@@ -1,1 +1,6 @@
 "use strict";
+// De esta manera le indico que va a recibir un dato de tipo numero pero que me va a devolver un string
+function getNumber(n1) {
+    return "El número es: " + n1;
+}
+console.log(getNumber(12));
---
+++ Typescript-y-POO/js/04-funciones.ts
@@ -0,0 +1,6 @@
+// De esta manera le indico que va a recibir un dato de tipo numero pero que me va a devolver un string
+function getNumber(n1: number): string {
+	return "El número es: " + n1;
+}
+
+console.log(getNumber(12));

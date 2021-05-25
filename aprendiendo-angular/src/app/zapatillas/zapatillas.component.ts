aaed48955a1e37f524e9e26b1c7df10a41eb85aa
Cristian Uhrig <cristianuhrig@gmail.com>
2021-05-25 17:37:00 -0300

    ngIf, ngFor, ngStyle

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -6,20 +6,33 @@ import { Zapatilla } from '../models/zapatilla';
   templateUrl: './zapatillas.component.html',
 })
 export class ZapatillasComponent implements OnInit {
-  public titulo!: string;
-  public listado!: string;
+  public titulo: string;
+  public listado: string;
   public zapatillas: Array<Zapatilla>;
+  public marcas!: string[];
   constructor() {
+    this.marcas = new Array();
     this.titulo = 'Componente de Zapatillas';
     this.listado = 'Listado de las zapatillas más populares';
     this.zapatillas = [
       new Zapatilla('Rebook classic', 3900, 'Rebook', 'Negro', true),
-      new Zapatilla('Nike sport', 4200, 'Nike', 'Blanco', true),
+      new Zapatilla('Nike sport', 4200, 'Nike', 'Blanco', false),
+      new Zapatilla('Adidas hunter', 2500, 'Adidas', 'Mostaza', true),
       new Zapatilla('DC Urban', 5700, 'DC', 'Gris', true),
       new Zapatilla('Adidas runner', 3500, 'Adidas', 'Negro', true),
     ];
   }
   ngOnInit() {
     console.log(this.zapatillas);
+    this.getMarcas();
+  }
+
+  getMarcas() {
+    this.zapatillas.forEach((zapatilla) => {
+      if (this.marcas.indexOf(zapatilla.marca) < 0) {
+        this.marcas.push(zapatilla.marca);
+      }
+    });
+    console.log(this.marcas);
   }
 }

c6db7b2e0184b1644d1c9b80f24d027f0a0b6825
Cristian Uhrig <cristianuhrig@gmail.com>
2021-05-29 11:45:30 -0300

    routings, links

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { CursosComponent } from './cursos/cursos.component';
     VideojuegoComponent,
     ZapatillasComponent,
     CursosComponent,
+    HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, routing],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

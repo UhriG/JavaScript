45a4b6f7c324d6d5606ad9b359a2cb2ca8688c78
Cristian Uhrig <cristianuhrig@gmail.com>
2021-05-30 14:32:52 -0300

    ngTemplate if else

--- aprendiendo-angular/src/app/home/home.component.ts
+++ aprendiendo-angular/src/app/home/home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public identificado!: boolean;
+  constructor() {
+    this.identificado = false;
   }
 
+  ngOnInit(): void {}
+
+  setIdentificado() {
+    this.identificado = true;
+  }
+  unsetIdentificado() {
+    this.identificado = false;
+  }
 }
